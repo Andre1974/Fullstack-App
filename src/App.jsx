082b58e3b7be56f977b9ecd16d1f97071e2d7f83
Andre Semm <andre.semm@dci-student.org>
2025-02-13 13:14:51 +0100

    FAILED

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import './App.scss';
-import ListItem from './components/ListItem';
-import { API_URL } from './config';
-const BASE_URL = API_URL // Hier steht die Backend-URL
+import { useState, useEffect } from "react";
+import "./App.scss";
+import ListItem from "./components/ListItem";
+import { API_URL } from "./config";
+const BASE_URL = API_URL; // Hier steht die Backend-URL
 
 function App() {
   const [task, setTask] = useState("");
@@ -10,15 +10,15 @@ function App() {
 
   useEffect(() => {
     getData();
-  }, [])
+  }, []);
 
-  async function getData(){
+  async function getData() {
     const response = await fetch(BASE_URL);
-    const {data} = await response.json();
-    console.log({data});
-    setList(data)
-      // Alle Tasks abrufen
-      // setList() - hier sollen die geholten Daten im State gespeichert werden
+    const { data } = await response.json();
+    console.log({ data });
+    setList(data);
+    // Alle Tasks abrufen
+    // setList() - hier sollen die geholten Daten im State gespeichert werden
   }
 
   const handleNewTask = (evt) => {
@@ -30,20 +30,19 @@ function App() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ task }),
-    })
-      .then((res) => {
-        console.log({ res });
-        getData();
-      })
+    }).then((res) => {
+      console.log({ res });
+      getData();
+    });
 
     // Daten für neuen Task sollen an das Backend geschickt werden
     // Anschließend müssen die aktualisierten Daten erneut abgerufen werden
-  }
+  };
 
   const handleDeleteAll = (evt) => {
     // Hier sollen alle Daten gelöscht werden
     // Anschließend müssen die aktualisierten Daten abgerufen werden
-  }
+  };
 
   return (
     <div className="app">
@@ -51,22 +50,33 @@ function App() {
       <form action="">
         <label>
           Add a new task
-          <input type="text" value={task} onChange={(evt) => setTask(evt.target.value)} />
+          <input
+            type="text"
+            value={task}
+            onChange={(evt) => setTask(evt.target.value)}
+          />
         </label>
-        <div className='buttonWrapper'>
+        <div className="buttonWrapper">
           <button onClick={handleNewTask}>New task</button>
           <button onClick={handleDeleteAll}>Delete all</button>
         </div>
       </form>
-      <div className='listWrapper'>
+      <div className="listWrapper">
         <ul>
-          {list.map((el, index) => (
-            <ListItem key={index} task={el} list={list} setList={setList} getData={getData} />
-          ))}
+          {Array.isArray(list) &&
+            list.map((el, index) => (
+              <ListItem
+                key={index}
+                task={el}
+                list={list}
+                setList={setList}
+                getData={getData}
+              />
+            ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
